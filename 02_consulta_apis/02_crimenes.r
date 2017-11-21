4f88f0c756d7da6fd8364732366d7b00d104e397
Sergio Buitrago Albarrán <sergio.buitrago@bluetab.net>
2017-11-22 00:44:56 +0100

    no message

--- 02_consulta_apis/02_crimenes.r
+++ 02_consulta_apis/02_crimenes.r
@@ -7,9 +7,16 @@ res_d <- content(res)
 print(res_d)
 
 
+lond <- GET("https://data.police.uk/api/crimes-at-location?date=2017-04&lat=51.4965946&lng=-0.1436476")
+lond_d <- content(lond)
 
-lond <- GET("https://data.police.uk/api/crimes-at-location?date=2017-04&lat=51.4965946&lon=-0.1436476")
-lond_d <- content(res)
-print(lond_d)
+categorias <- c()
+valores <- c()
 
-rapply(lond_d, sum, lond_d[["category"]])
+for (i in 1:length(lond_d)) {
+  categorias <- c(categorias, lond_d[[i]]$category)
+  valores <- c(valores, 1)
+  i <- i + 1 
+}
+
+aggregate(valores, by=list(categorias), FUN=sum)
